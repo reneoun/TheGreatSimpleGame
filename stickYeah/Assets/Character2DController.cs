f146841516d8e4e1de5676313a0f3e29def0d65b
reneoun <43538982+reneoun@users.noreply.github.com>
2020-11-02 19:38:59 +0100

    The Begin
    Player
     - can move.

---
+++ stickYeah/Assets/Character2DController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character2DController : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+    public Animator animator;
+
+    private Rigidbody2D _rigidbody;
+    private Vector3 characterScale;
+    private bool lookright = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        characterScale = transform.localScale;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        animator.SetFloat("speed", Mathf.Abs(movement));
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0,JumpForce), ForceMode2D.Impulse);
+        }
+
+        characterScale.x = Input.GetAxis("Horizontal") < 0 ? -1 : characterScale.x;
+        characterScale.x = Input.GetAxis("Horizontal") > 0 ? 1 : characterScale.x;
+        transform.localScale = characterScale;
+    }
+}
